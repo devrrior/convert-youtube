7ddf1903148d88f2bf8cba8f973cefef048b2d70
Fernando Guerrero <devrrior@gmail.com>
2021-05-05 00:39:22 -0500

    I hope this works

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
 from youtube import YoutubeDownload
 
-url = input("Give me the video's URL: ")
+def main():
+    url = input("Give me the video's URL: ")
 
-loop = True
-while loop == True:
-    option = int(input("""
-    What would you download?
-    1. Video
-    2. Audio
-    3. Change URL
-    4. Exit
+    loop = True
+    while loop == True:
+        option = int(input("""
+        What would you download?
+        1. Video
+        2. Audio
+        3. Change URL
+        4. Exit
 
-    Type: """))
+        Type: """))
 
-    if option == 1:
-        video = YoutubeDownload(url)
-        video.download_video()
-        print("\nDownload complete!")
-    elif option == 2:
-        audio = YoutubeDownload(url)
-        audio.download_audio()
-        print("\nDownload complete!")
-        loop = False
-    elif option == 3:
-        url = input("Give me the video's URL: ")
-    elif option == 4:
-        loop = False
-    else:
-        print("Type one option")
+        if option == 1:
+            video = YoutubeDownload(url)
+            video.download_video()
+            print("\nDownload complete!")
+        elif option == 2:
+            audio = YoutubeDownload(url)
+            audio.download_audio()
+            print("\nDownload complete!")
+            loop = False
+        elif option == 3:
+            url = input("Give me the video's URL: ")
+        elif option == 4:
+            loop = False
+        else:
+            print("Type one option")
+
+if __name__ == "__main__":
+    main()
